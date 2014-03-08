510f29754f2d99250f9e912d70c5c3242df96fec
Matt Wilson <mhw@hypomodern.com>
2014-03-08 11:07:51 -0500

    tweak unicorn for heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,33 +1,30 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-worker_processes (rails_env == 'development' ? 2 : 6)
-timeout 60
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 30
 preload_app true
 
 case rails_env
-when 'production', 'staging'
-  pid '/var/www/achievements/shared/pids/unicorn.pid'
-  listen "/var/www/achievements/tmp/sockets/#{rails_env}.sock", :backlog => 2048
-else
+when 'development'
   listen 3002
   listen "#{`pwd`.strip}/tmp/sockets/#{rails_env}.sock"
 end
 
 before_fork do |server, worker|
-  # Using this method we get 0 downtime deploys.
-  old_pid = '/var/www/achievements/shared/pids/unicorn.pid.oldbin'
-
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
   end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
 end
 
 after_fork do |server, worker|
-  # Signal.trap 'TERM' do
-  #   puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
-  # end
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
 end
