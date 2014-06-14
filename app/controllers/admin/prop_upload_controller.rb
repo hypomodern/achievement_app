7cd47fd8c2c6e4e1c675f8968450f79dc47d6e64
Matt Wilson <mhw@hypomodern.com>
2014-06-13 22:19:31 -0400

    Streamline prop upload routing.

--- app/controllers/admin/prop_upload_controller.rb
+++ app/controllers/admin/prop_upload_controller.rb
@@ -3,7 +3,7 @@ module Admin
     def handle_upload_success
       @prop = Prop.find params[:id]
       @prop.update_attribute :key, params[:key]
-      redirect_to admin_prop_url(@prop)
+      redirect_to admin_chapter_url(@prop.chapter_id)
     end
   end
 end
